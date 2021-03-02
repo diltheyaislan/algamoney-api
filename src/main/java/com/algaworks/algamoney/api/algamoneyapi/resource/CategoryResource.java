8f1ae3088a44fe5f1f7ec8db6bbf87b22cb0168d
Dilthey <diltheyaislan@gmail.com>
2021-03-01 22:29:57 -0300

    delete category

--- src/main/java/com/algaworks/algamoney/api/algamoneyapi/resource/CategoryResource.java
+++ src/main/java/com/algaworks/algamoney/api/algamoneyapi/resource/CategoryResource.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,5 +55,11 @@ public class CategoryResource {
 			.findById(id)
 			.map(categoria -> ResponseEntity.ok(categoria))
 			.orElse(ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		categoryRepository.deleteById(id);
 	}
 }
