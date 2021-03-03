22e00d31e2eec1e4bfd9a95dfee02690e0e92278
Dilthey <diltheyaislan@gmail.com>
2021-03-02 21:46:37 -0300

    refactoring imports

--- src/main/java/com/algaworks/algamoney/api/algamoneyapi/resource/PersonResource.java
+++ src/main/java/com/algaworks/algamoney/api/algamoneyapi/resource/PersonResource.java
@@ -1,7 +1,6 @@
 package com.algaworks.algamoney.api.algamoneyapi.resource;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -11,10 +10,8 @@ import com.algaworks.algamoney.api.algamoneyapi.model.Person;
 import com.algaworks.algamoney.api.algamoneyapi.repository.PersonRepository;
 import com.algaworks.algamoney.api.algamoneyapi.service.PersonService;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
