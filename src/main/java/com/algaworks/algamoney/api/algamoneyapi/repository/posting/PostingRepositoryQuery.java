a2aaa7dde6f55e7d8014e69ae8204ff353e333f6
Dilthey <diltheyaislan@gmail.com>
2021-03-11 21:27:00 -0300

    refactoring import

--- src/main/java/com/algaworks/algamoney/api/algamoneyapi/repository/posting/PostingRepositoryQuery.java
+++ src/main/java/com/algaworks/algamoney/api/algamoneyapi/repository/posting/PostingRepositoryQuery.java
@@ -1,7 +1,5 @@
 package com.algaworks.algamoney.api.algamoneyapi.repository.posting;
 
-import java.util.List;
-
 import com.algaworks.algamoney.api.algamoneyapi.model.Posting;
 import com.algaworks.algamoney.api.algamoneyapi.repository.filter.PostingFilter;
 
